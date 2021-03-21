f1cb037112e34f30a8e1187be462e756a7cd13c1
nwa24 <nwa24@sfu.ca>
2021-03-21 11:47:10 -0700

    refactored code to use EduMonitor SDK

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -1,100 +1,56 @@
+const client = Client.create('https://5ga1qnpnq0.execute-api.us-east-1.amazonaws.com/jsonrpc');
+
+async function addTask(event) {
+  event.preventDefault(); // prevents the page from refreshing
+
+  const formData = getFormData('add-task-form');
+
+  try {
+    // classroom, student, addedBy is hard coded for now
+    // done will always be false when adding a new task
+    // returns the task object
+    const task = await client.Tasks.addTask(1, formData.title, formData.deadline, 3, false, 1);
+    console.log(JSON.parse(task));
+  } catch (error) {
+    alert('error ' + error.message);
+  }
+}
+
+async function deleteTask(event) {
+  event.preventDefault(); //prevent the page from refreshing
+
+  const formData = getFormData('delete-task-form'); // TODO: get task ID from the form
+
+  try {
+    // return the ID of the deleted task
+    const deletedTask = await client.Tasks.deleteTask('afe5e1a4-a325-4119-a611-38d8f38dd996');
+    alert('success ' + deletedTask);
+  } catch (error) {
+    alert('error ' + error.message);
+  }
+}
+
+async function listTasks() {
+  try {
+    // can query for tasks by studentId and classroomId (optional)
+    const tasks = await client.Tasks.listTasks(3);
+    console.log(JSON.parse(tasks));
+  } catch (error) {
+    alert('error ' + error.message);
+  }
+}
+
 // to load the tasks when the page first loads
 // $(document).ready(function () {
 //   listTasks();
 // });
 
-function createHeaders() {
-  const headers = new Headers();
-  headers.append("Content-Type", "application/json");
-  return headers;
-}
-
 function getFormData(formID) {
   const form = document.getElementById(formID);
   return Object.values(form).reduce((obj, field) => {
-    if (field.type != "submit") {
+    if (field.type != 'submit') {
       obj[field.name] = field.value;
     }
     return obj;
   }, {});
-}
-
-function deleteTask(event) {
-  event.preventDefault(); // debugging -  prevent the page from refreshing
-  const formData = getFormData("delete-task-form");
-
-  const headers = createHeaders();
-
-  const data = JSON.stringify({
-    taskId: "b3d8b122-67ab-4e29-8f81-6ad4059974d9", // formData.task - for now the value is hardcoded for
-  });
-
-  const requestOptions = {
-    method: "DELETE",
-    headers: headers,
-    body: data,
-    redirect: "follow",
-  };
-
-  fetch("https://xckwrl3df7.execute-api.us-east-1.amazonaws.com/dev", requestOptions)
-    .then((response) => response.text())
-    .then((data) => {
-      console.log(data.body);
-    })
-    .catch((error) => console.log("error", error.message));
-}
-
-function listTasks() {
-  const headers = createHeaders();
-
-  const requestOptions = {
-    method: "GET",
-    headers: headers,
-  };
-
-  // student id is hard coded for now
-  // NOTE: can also filter by classroom /studentId/classroomId
-  fetch("https://xckwrl3df7.execute-api.us-east-1.amazonaws.com/dev/2", requestOptions)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => console.log(JSON.parse(data.body)))
-    .catch((error) => console.log("error", error.message));
-}
-
-function addTask(event) {
-  event.preventDefault(); //debugging - prevent the page from refreshing
-
-  // getting the form data
-  const formData = getFormData("add-task-form");
-
-  // instantiate a headers object
-  const headers = createHeaders();
-
-  const data = JSON.stringify({
-    classroom: 2, // hardcoded int
-    title: formData.title,
-    deadline: formData.deadline,
-    student: 2, // hardcoded int
-    done: false,
-    addedBy: 1, //hardcoded int
-  });
-
-  // create a JSON object with parameterrs for API call
-  const requestOptions = {
-    method: "POST",
-    headers: headers,
-    body: data,
-    redirect: "follow",
-  };
-
-  // make API call and use promises to get response
-  fetch("https://xckwrl3df7.execute-api.us-east-1.amazonaws.com/dev", requestOptions)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      console.log(JSON.parse(data.body));
-    })
-    .catch((error) => console.log("error", error.message));
 }
